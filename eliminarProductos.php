edbc31e12a966377ea83f8a1aff24a63e99b91b0
yorladymontoya <70727561+yorladymontoya@users.noreply.github.com>
2020-11-13 14:21:43 -0500

    Update eliminarProductos.php

--- eliminarProductos.php
+++ eliminarProductos.php
@@ -6,11 +6,7 @@ include("BaseDatos.php");
 $idEliminar=($_GET["id"]);
 $transaccion= new BaseDatos();
 
-//3. crear la consulta SQL para eliminar
-
 $consultaSQL="DELETE FROM productos WHERE idProducto='$idEliminar'";
-
-//4. Utilizar el metodo para eliminar datos
 
 $transaccion->eliminarProductos($consultaSQL);
 
