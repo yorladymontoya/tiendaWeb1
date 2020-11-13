698b42c247b7b9d1ba18a11133f0e8f443b82b7f
yorladymontoya <70727561+yorladymontoya@users.noreply.github.com>
2020-11-13 14:22:25 -0500

    Update editarProductos.php

--- editarProductos.php
+++ editarProductos.php
@@ -15,17 +15,11 @@ if(isset($_POST["botonEditar"])){
     $marca=$_POST['marcaEditar'];
     $precio=$_POST['precioEditar'];
 
-//4. Consulta para editar un registro
-
 $consultaSQL="UPDATE productos SET nombre='$nombre',marca='$marca',precio='$precio',descripcion='$descripcion' WHERE idProducto='$id'";
-
-//5. Utilizar el metodo Editar
 
 $transaccion->editarProductos($consultaSQL);
 
 }
-
 header("Location:listarProductos.php");
-
 
 ?>
